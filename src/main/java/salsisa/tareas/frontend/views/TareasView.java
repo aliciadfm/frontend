c77ae8d34e099910164f97778d2edce1566fdac2
Alicia de Felipe <dfmalicia@gmail.com>
2025-04-12 18:04:32 +0200

    cabecera de tareasview

--- src/main/java/salsisa/tareas/frontend/views/TareasView.java
+++ src/main/java/salsisa/tareas/frontend/views/TareasView.java
@@ -1,5 +1,6 @@
 package salsisa.tareas.frontend.views;
 
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -7,5 +8,19 @@ import com.vaadin.flow.router.Route;
 @PageTitle("SH - Visualizar tareas")
 @Route(value="tareas", layout = MainLayout.class)
 public class TareasView extends VerticalLayout {
+    public TareasView() {
+        setSpacing(false);
+        setPadding(false);
 
+        getStyle().set("padding", "0 5%");
+        createHeader();
+    }
+
+    public void createHeader() {
+        VerticalLayout headingDiv = new VerticalLayout();
+        headingDiv.setWidth("100%");
+        H1 title = new H1("Lista de tareas");
+        headingDiv.add(title);
+        add(headingDiv);
+    }
 }
