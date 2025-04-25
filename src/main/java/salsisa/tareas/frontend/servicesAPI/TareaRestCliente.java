378c8de916fc94c0627eafa514bb8dd47926e805
marta-tech <60746429+marta-tech@users.noreply.github.com>
2025-04-25 11:46:53 +0200

    filtros tarea

--- src/main/java/salsisa/tareas/frontend/servicesAPI/TareaRestCliente.java
+++ src/main/java/salsisa/tareas/frontend/servicesAPI/TareaRestCliente.java
@@ -4,7 +4,12 @@ package salsisa.tareas.frontend.servicesAPI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+import salsisa.tareas.frontend.dto.Estado;
 import salsisa.tareas.frontend.dto.TareaDTO;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class TareaRestCliente extends ClienteRestBase<TareaDTO> {
@@ -14,5 +19,14 @@ public class TareaRestCliente extends ClienteRestBase<TareaDTO> {
     @Autowired
     public TareaRestCliente(RestTemplate restTemplate) {
         super(restTemplate, BASE_URL, TareaDTO.class);
+    }
+
+    public List<TareaDTO> filtrarPorEstados(List<Estado> estados) {
+        String url = UriComponentsBuilder.fromHttpUrl(BASE_URL + "/filtro")
+                .queryParam("estados", estados.toArray())
+                .toUriString();
+
+        TareaDTO[] tareasArray = getRestTemplate().getForObject(url, TareaDTO[].class);
+        return Arrays.asList(tareasArray);
     }
 }
