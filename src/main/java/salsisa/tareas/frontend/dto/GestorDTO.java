2803a14567e820a455750b2cfec8dd961951b394
marta-tech <60746429+marta-tech@users.noreply.github.com>
2025-05-21 08:55:38 +0200

    gestor login

---
+++ src/main/java/salsisa/tareas/frontend/dto/GestorDTO.java
@@ -0,0 +1,15 @@
+package salsisa.tareas.frontend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GestorDTO {
+    private Long idGestor;
+    private String usuario;
+}
+
+
---
+++ src/main/java/salsisa/tareas/frontend/servicesAPI/GestorRestCliente.java
@@ -0,0 +1,45 @@
+package salsisa.tareas.frontend.servicesAPI;
+import org.springframework.http.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import salsisa.tareas.frontend.dto.GestorDTO;
+import salsisa.tareas.frontend.dto.GestorLoginDTO;
+
+@Service
+public class GestorRestCliente {
+
+        private final RestTemplate restTemplate;
+
+        public GestorRestCliente(RestTemplate restTemplate) {
+            this.restTemplate = restTemplate;
+        }
+
+        public GestorDTO login(String usuario, String contrasena) {
+            String url = "http://localhost:8080/api/gestores/login";
+
+            GestorLoginDTO loginDTO = new GestorLoginDTO();
+            loginDTO.setUsuario(usuario);
+            loginDTO.setContrasena(contrasena);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            HttpEntity<GestorLoginDTO> request = new HttpEntity<>(loginDTO, headers);
+
+            try {
+                ResponseEntity<GestorDTO> response = restTemplate.postForEntity(url, request, GestorDTO.class);
+
+                if (response.getStatusCode() == HttpStatus.OK) {
+                    return response.getBody();
+                } else {
+                    throw new RuntimeException("Error al autenticar: " + response.getStatusCode());
+                }
+
+            } catch (Exception e) {
+                throw new RuntimeException("Autenticación fallida: " + e.getMessage());
+            }
+        }
+    }
+
+
