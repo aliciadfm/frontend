21c4cdb64d24a650ef4b72efc57bc766eeaceb39
sergio <scuecer@upv.edu.es>
2025-05-21 22:03:02 +0200

    Terminada vista tareas

--- src/main/java/salsisa/tareas/frontend/views/MainLayout.java
+++ src/main/java/salsisa/tareas/frontend/views/MainLayout.java
@@ -3,6 +3,7 @@ package salsisa.tareas.frontend.views;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.shared.Tooltip;
 import com.vaadin.flow.router.RouterLayout;
 import com.vaadin.flow.router.RouterLink;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
@@ -16,17 +17,35 @@ public class MainLayout extends AppLayout implements RouterLayout {
         RouterLink needs = new RouterLink();
         RouterLink createTask = new RouterLink();
         RouterLink tasks = new RouterLink();
+        RouterLink logout= new RouterLink();
 
-        needs.add(new Image("icons/needsVisualization.png", "Necesidades"));
+        Image needsImage = new Image("icons/needsVisualization.png", "Necesidades");
+        needs.add(needsImage);
         needs.setRoute(NeedsView.class);
+        Tooltip needsText = Tooltip.forComponent(needsImage);
+        needsText.setText("Seleccionar necesidades");
 
-        createTask.add(new Image("icons/taskCreation.png", "Crear tarea"));
+        Image createTaskImage = new Image("icons/taskCreation.png", "Crear tarea");
+        createTask.add(createTaskImage);
         createTask.setRoute(CreateTaskView.class);
+        Tooltip createTaskText = Tooltip.forComponent(createTaskImage);
+        createTaskText.setText("Crear tarea");
 
-        tasks.add(new Image("icons/tareasVisualization.png", "Ver tareas"));
+        Image taskViewImage = new Image("icons/tareasVisualization.png", "Ver tareas");
+        tasks.add(taskViewImage);
         tasks.setRoute(TareasView.class);
+        Tooltip tareasViewText = Tooltip.forComponent(taskViewImage);
+        tareasViewText.setText("Lista de tareas");
 
-        VerticalLayout menu = new VerticalLayout(needs, createTask, tasks);
+        Image logoutImage = new Image("icons/logout.png", "Logout");
+        logoutImage.setHeight("64px");
+        logoutImage.setWidth("64px");
+        logout.add(logoutImage);
+        logout.setRoute(LoginView.class);
+        Tooltip logoutText = Tooltip.forComponent(logoutImage);
+        logoutText.setText("Cerrar sesion");
+
+        VerticalLayout menu = new VerticalLayout(needs, createTask, tasks, logout);
         menu.getStyle().set("background-color", "#B64040").setHeight("100%"); // Cambia el color
         menu.setAlignItems(Alignment.CENTER);
         menu.getStyle().set("padding-top", "10%");
--- src/main/java/salsisa/tareas/frontend/views/TareasView.java
+++ src/main/java/salsisa/tareas/frontend/views/TareasView.java
@@ -1,20 +1,21 @@
 package salsisa.tareas.frontend.views;
 
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.shared.Tooltip;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 import salsisa.tareas.frontend.components.TaskCard;
-import salsisa.tareas.frontend.components.TaskFilters;
 import salsisa.tareas.frontend.components.TaskFilters2;
 import salsisa.tareas.frontend.dto.Estado;
 import salsisa.tareas.frontend.dto.FiltroTareaDTO;
 import salsisa.tareas.frontend.dto.TareaResumenDTO;
 import salsisa.tareas.frontend.servicesAPI.TareaRestCliente;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -40,7 +41,14 @@ public class TareasView extends VerticalLayout {
     public void createHeader() {
         VerticalLayout headingDiv = new VerticalLayout();
         headingDiv.setWidth("100%");
+        headingDiv.setPadding(false);
+        headingDiv.setSpacing(false);
+
         H1 title = new H1("Lista de tareas");
+        title.getStyle()
+                .set("margin-top", "40px")
+                .set("margin-bottom", "20px");
+
         headingDiv.add(title);
         add(headingDiv);
     }
@@ -48,13 +56,16 @@ public class TareasView extends VerticalLayout {
     public void createCentralContainer() {
         HorizontalLayout centralContainer = new HorizontalLayout();
         centralContainer.setWidth("100%");
+        centralContainer.setSpacing(true);
+        centralContainer.setPadding(false);
+        centralContainer.getStyle().set("margin-top", "0px");
 
         taskFilters2 = new TaskFilters2();
-
         taskContainer = createTasksContainer();
 
-        add(centralContainer);
         centralContainer.add(taskContainer, taskFilters2);
+        add(centralContainer);
+
         taskFilters2.getBotonFiltrar().addClickListener(event -> {
             FiltroTareaDTO filtro = taskFilters2.getFiltroActual();
             actualizarListaTareasConFiltro(filtro);
@@ -64,6 +75,10 @@ public class TareasView extends VerticalLayout {
     public VerticalLayout createTasksContainer() {
         VerticalLayout taskcreated = new VerticalLayout();
         taskcreated.setWidth("100%");
+        taskcreated.setPadding(false);
+        taskcreated.setSpacing(true);
+        taskcreated.getStyle().set("margin-top", "0px");
+        taskcreated.getStyle().set("gap", "10px");
 
         FiltroTareaDTO filtro = new FiltroTareaDTO();
         filtro.setEstados(Arrays.asList(
@@ -75,8 +90,28 @@ public class TareasView extends VerticalLayout {
 
         List<TareaResumenDTO> listaTareas  = tareaRestCliente.obtenerTodos(filtro);
         for (TareaResumenDTO tarea : listaTareas) {
+            HorizontalLayout task = new HorizontalLayout();
+            task.setWidth("100%");
+            task.setAlignItems(Alignment.CENTER);
+            Div espacio = new Div();
+            espacio.setWidth("60px"); // o el tamaño necesario para tu layout
+            espacio.getStyle().set("padding-right", "3px"); // espacio con el contenido principal
+
+            if (tarea.isAviso()) {
+                Image warn = new Image("icons/Warning.png", "NoSuficientes");
+                warn.setWidth("60px");
+                warn.setHeight("60px");
+
+                Tooltip nosuf = Tooltip.forComponent(warn);
+                nosuf.setText("Demasiados voluntarios rechazaron la tarea");
+
+                espacio.add(warn);
+            }
+
+            task.add(espacio);
             TaskCard taskCard = new TaskCard(tarea);
-            taskcreated.add(taskCard);
+            task.add(taskCard);
+            taskcreated.add(task);
         }
         return taskcreated;
     }
